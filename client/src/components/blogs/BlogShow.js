7bce200773fc84ab099201b5635c945c0cf1588c
rajapattanayak <pattanayak.raja@gmail.com>
2019-01-17 18:37:08 +0530

    s3 image upload

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBlog } from "../../actions";
 
 class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
-
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return (
+        <img
+          src={
+            "https://s3.ap-south-1.amazonaws.com/praja-blog-bucket/" +
+            this.props.blog.imageUrl
+          }
+        />
+      );
+    }
+  }
   render() {
     if (!this.props.blog) {
-      return '';
+      return "";
     }
 
     const { title, content } = this.props.blog;
@@ -18,6 +29,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
@@ -27,4 +39,7 @@ function mapStateToProps({ blogs }, ownProps) {
   return { blog: blogs[ownProps.match.params._id] };
 }
 
-export default connect(mapStateToProps, { fetchBlog })(BlogShow);
+export default connect(
+  mapStateToProps,
+  { fetchBlog }
+)(BlogShow);
--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://praja:test123@ds253094.mlab.com:53094/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIX6UKYIGYG7WRI3Q',
+  secretAccessKey: ''
 };
