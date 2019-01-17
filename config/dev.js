9cbb7990d3e7d8e697088a1e15ed3f345522fa7c
rajapattanayak <pattanayak.raja@gmail.com>
2019-01-17 19:05:07 +0530

    Minor fix

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://praja:test123@ds253094.mlab.com:53094/blog_dev',
+  mongoURI: '',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
   accessKeyId: 'AKIAIX6UKYIGYG7WRI3Q',
